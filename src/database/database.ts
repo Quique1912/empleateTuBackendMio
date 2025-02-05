fddad0a18b66ed7815b7d73a112e54b650aea4d6
Quique1912 <quique190120@gmail.com>
2025-02-05 12:02:08 +0100

    8.0 version

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { createClient } from '@libsql/client'
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
 
-const libsql = createClient({
+export const libsql = createClient({
   url: `${process.env.TURSO_DATABASE_URL}`,
   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 })
--- src/routes/offer.routes.ts
+++ src/routes/offer.routes.ts
@@ -1,6 +1,7 @@
 import { isAdmin } from "../middlewares/isAdmin.middleware";
 import { OfferController } from "../controllers/offer.controller";
 import { Router } from "express";
+import { isAuthenticate } from "../middlewares/auth.middleware";
 
 const router = Router()
 
@@ -8,16 +9,16 @@ const router = Router()
 router.get('/', OfferController.getAll)
 router.get('/:id', OfferController.getById)
 // POST añadir una oferta nueva localhost:3000/api/offerts/ {body}
-router.post('/', OfferController.create)
+router.post('/', isAuthenticate, OfferController.create)
 //Delete Borrar una oferta localhost:3000/api/offerts/xxxx
-router.post('/:id', isAdmin, OfferController.delete)
+router.post('/:id', isAuthenticate, OfferController.delete)
 //PUT modificar una oferta localhost:3000/api/offerts/xxxx {body}
-router.put('/:id', OfferController.update)
+router.put('/:id', isAuthenticate, OfferController.update)
 
 
 // calificamos una oferta x
-router.post('/:id/rate/', OfferController.rate)
+router.post('/:id/rate/', isAuthenticate, OfferController.rate)
 //Vemos que calificacion (total) se le ha dado a una oferta
-router.get('/:id/rate/', OfferController.getRate)
+router.get('/:id/rate/', isAuthenticate, OfferController.getRate)
 
 export default router
