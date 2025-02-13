e72282ac9f123c2832cf60a7e6f6a120bf135d73
Quique1912 <quique190120@gmail.com>
2025-02-13 09:56:27 +0100

    al fin funciona

--- prisma/migrations/20250213083447_init/migration.sql
+++ prisma/migrations/20250213083447_init/migration.sql
@@ -8,7 +8,7 @@ CREATE TABLE "User" (
     "role" TEXT,
     "course" TEXT,
     "active" BOOLEAN NOT NULL DEFAULT true,
-    "accepNotifications" BOOLEAN NOT NULL DEFAULT true,
+    "acceptNotifications" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
@@ -27,8 +27,8 @@ CREATE TABLE "Offer" (
     "updatedAt" DATETIME NOT NULL,
     "idCategory" INTEGER,
     "idUserCreator" INTEGER NOT NULL,
-    CONSTRAINT "Offer_idUserCreator_fkey" FOREIGN KEY ("idUserCreator") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Offer_idCategory_fkey" FOREIGN KEY ("idCategory") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    CONSTRAINT "Offer_idCategory_fkey" FOREIGN KEY ("idCategory") REFERENCES "Category" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Offer_idUserCreator_fkey" FOREIGN KEY ("idUserCreator") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -48,8 +48,8 @@ CREATE TABLE "Rate" (
     "updatedAt" DATETIME NOT NULL,
 
     PRIMARY KEY ("idUser", "idOffer"),
-    CONSTRAINT "Rate_idOffer_fkey" FOREIGN KEY ("idOffer") REFERENCES "Offer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Rate_idUser_fkey" FOREIGN KEY ("idUser") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Rate_idUser_fkey" FOREIGN KEY ("idUser") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Rate_idOffer_fkey" FOREIGN KEY ("idOffer") REFERENCES "Offer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
